ad2253d3e07fb5fcf5293fa279b4bc0dfbc1d201
mikolajczykb <uncharted.mikolajczyk4@gmail.com>
2021-03-04 15:00:21 +0100

    added graph conversion module
    Implemented convertions from all methods of representing graphs: adjacency matrix, adjacency list, incidency matrix

--- project1/__main__.py
+++ project1/__main__.py
@@ -1,5 +1,6 @@
 from graph_generation import generate_N_P_graph, generate_N_L_graph
-
+from graph_conversion import *
 
 if __name__ == "__main__":
-    print(generate_N_L_graph(5, 5))
+    graph_N_L = generate_N_L_graph(5, 5)
+    
