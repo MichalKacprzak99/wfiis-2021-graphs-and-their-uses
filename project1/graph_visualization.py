8e84dc0876e5ea2353b85c98e167b969c1fa1b34
Michał Ambroży <michal@ambrozy.pl>
2021-03-04 19:14:02 +0100

    Finish working

--- project1/graph_visualization.py
+++ project1/graph_visualization.py
@@ -6,25 +6,22 @@ from vpython import *
 
 
 def visualize_graph(graph):
-    scene = canvas(width=500, height=500)
-
-    print(graph)
+    scene = canvas(width=1000, height=800)
 
     (verticeNumber, verticeNumber) = graph.shape
 
-    rfactor = 5
-    radius = verticeNumber/2
+    rfactor = 50
+    radius = -(1/4) * verticeNumber + 100
     vertices = []
-    r = rfactor * verticeNumber
+    r = rfactor * verticeNumber + 100
     x0 = 0
     y0 = 0
 
-    thetaFactor = 360 / verticeNumber
+    t = np.linspace(0, 2 * math.pi, verticeNumber + 1 )
     theta = 0
     for i in range(verticeNumber):
-        theta = i * radius * thetaFactor
-        positionVector = vector(x0 + r * math.cos(theta),
-                                y0 + r * math.sin(theta), 0)
+        positionVector = vector(x0 + (r+radius) * math.sin(t[i]),
+                                y0 + (r+radius) * math.cos(t[i]), 0)
         label(pos=positionVector, text=str(i+1), opacity=0, box=False)
         vertices.append(sphere(pos=positionVector, radius=radius,
                                color=vec(rand.random(), rand.random(), rand.random())))
@@ -35,7 +32,7 @@ def visualize_graph(graph):
     z = [tuple(i) for i in np.unique(lista_sasiedztwa, axis=0)]
 
     for (first, second) in z:
-        curve(vertices[first].pos, vertices[second].pos, color=color.cyan)
+        curve(vertices[first].pos, vertices[second].pos, color=vec(rand.random(), rand.random(), rand.random()))
 
 
-visualize_graph(graph_generation.generate_N_L_graph(10, 10))
+visualize_graph(graph_generation.generate_N_L_graph(40, 10))
