403a915c14c614c658cea8c3244a9de97a89ba06
Michał Ambroży <michal@ambrozy.pl>
2021-03-18 18:23:21 +0100

    Little changes

--- project1/graph_visualization.py
+++ project1/graph_visualization.py
@@ -8,28 +8,28 @@ from vpython import *
 def visualize_graph(graph_by_matrix):
     scene = canvas(width=1000, height=800)
 
-    (verticeNumber, verticeNumber) = graph_by_matrix.shape
+    (vertexNumber, vertexNumber) = graph_by_matrix.shape
 
     rfactor = 50
-    radius = -(1/4) * verticeNumber + 100
-    vertices = []
-    r = rfactor * verticeNumber + 100
+    radius = -(1/4) * vertexNumber + 100
+    vertice = []
+    r = rfactor * vertexNumber + 100
     x0 = 0
     y0 = 0
 
-    t = np.linspace(0, 2 * math.pi, verticeNumber + 1)
-    for i in range(verticeNumber):
+    t = np.linspace(0, 2 * math.pi, vertexNumber + 1)
+    for i in range(vertexNumber):
         positionVector = vector(x0 + (r+radius) * math.sin(t[i]),
                                 y0 + (r+radius) * math.cos(t[i]), 0)
         label(pos=positionVector, text=str(i+1), opacity=0, box=False)
-        vertices.append(sphere(pos=positionVector, radius=radius,
+        vertice.append(sphere(pos=positionVector, radius=radius,
                                color=vec(rand.random(), rand.random(), rand.random())))
 
     result = np.where(graph_by_matrix == 1)
-    lista_sasiedztwa = np.sort(list(zip(result[0], result[1])))
+    listaa = np.sort(list(zip(result[0], result[1])))
 
-    z = [tuple(i) for i in np.unique(lista_sasiedztwa, axis=0)]
+    z = [tuple(i) for i in np.unique(listaa, axis=0)]
 
     for (first, second) in z:
-        curve(vertices[first].pos, vertices[second].pos, color=vec(rand.random(), rand.random(), rand.random()))
+        curve(vertice[first].pos, vertice[second].pos, color=vec(rand.random(), rand.random(), rand.random()))
     scene.capture("letnie_dranie_graph")
