3a8058ce3f42cc6233f3324de61fc366a2809d15
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-04-01 20:02:41 +0200

    fix graph generation

--- project1/graph_generation.py
+++ project1/graph_generation.py
@@ -1,24 +1,23 @@
 import numpy as np
-import random
 
 
 def generate_N_L_graph(vertices_number: int, edges_number: int):
-    generate_edges = list()
-    edge = random.choices(range(vertices_number), k=2)
-
-    while len(generate_edges) < edges_number:
-        while edge[0] >= edge[1] or edge in generate_edges:
-            edge = random.choices(range(vertices_number), k=2)
-        generate_edges.append(edge)
-
+    generated_edges = 0
     graph = np.zeros((vertices_number, vertices_number))
-    for edge in generate_edges:
+    edge = np.random.choice(vertices_number, size=2, replace=False)
+    while generated_edges < edges_number:
+        while graph[tuple(edge)] == 1:
+            edge = np.random.choice(range(vertices_number), size=2, replace=False)
+
         graph[tuple(edge)] = 1
-        
-    return graph + np.transpose(graph)
+        graph[tuple(edge[::-1])] = 1
+        generated_edges += 1
+
+    return graph
 
 
 def generate_N_P_graph(vertices_number: int, edges_probability: float):
-    graph = np.random.choice([0, 1], (vertices_number, vertices_number), p=[1-edges_probability, edges_probability])
+    graph = np.random.choice([0, 1], (vertices_number, vertices_number),
+                             p=[1-edges_probability, edges_probability], replace=False)
     graph = np.tril(graph, -1)
     return graph + np.transpose(graph)
