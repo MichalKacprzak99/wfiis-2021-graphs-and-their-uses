9a2de6a61b68f39951d494f7969eb9484e8b2b13
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-04-02 21:51:50 +0200

    create example main file

--- project1/__main__.py
+++ project1/__main__.py
@@ -2,31 +2,15 @@ from graph_generation import generate_N_P_graph, generate_N_L_graph
 from graph_visualization import visualize_graph
 from graph_conversion import *
 
-
-adj_matrix = [[0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-             [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-             [0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
-             [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0],
-             [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
-             [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
-             [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
-             [0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-             [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
-
-
 if __name__ == "__main__":
+    adj_matrix = generate_N_L_graph(1, 0)
 
-    adj_list = adj_matrix_to_list(adj_matrix)
-    print_adj_list(adj_list)
-    converted_adj_matrix_from_adj_list = adj_list_to_matrix(adj_list)
-    print(np.array_equal(np.asarray(adj_matrix), converted_adj_matrix_from_adj_list))
+    print("Macierz sąsiedzctwa")
+    print(adj_matrix)
+    print("Lista sąsiedzctwa")
+    print_adj_list(adj_matrix_to_list(adj_matrix))
+    print("Macierz incydencji")
+    print(adj_matrix_to_inc_matrix(adj_matrix))
+    visualize_graph(adj_matrix)
 
-    inc_matrix = adj_matrix_to_inc_matrix(adj_matrix)
-    print(inc_matrix)
-    converted_adj_matrix_from_inc_matrix = inc_matrix_to_adj_matrix(inc_matrix)
-    print(np.array_equal(np.asarray(adj_matrix), converted_adj_matrix_from_inc_matrix))
-
-    # visualize_graph(generate_N_L_graph(10, 15))
+    visualize_graph(generate_N_P_graph(10, 0.5))
