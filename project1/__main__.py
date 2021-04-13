3c4977961a6a7f54ba5f93f19cf0520b85d691df
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-04-13 10:19:06 +0200

    create final version of __main__.py

--- project1/__main__.py
+++ project1/__main__.py
@@ -5,12 +5,14 @@ from graph_conversion import *
 if __name__ == "__main__":
     adj_matrix = generate_N_L_graph(1, 0)
 
-    print("Macierz sąsiedzctwa")
+    print("Adjacency matrix")
     print(adj_matrix)
-    print("Lista sąsiedzctwa")
+    print()
+    print("Adjacency list")
+    print(type(adj_matrix_to_list(adj_matrix)))
     print_adj_list(adj_matrix_to_list(adj_matrix))
-    print("Macierz incydencji")
+    print()
+    print("Incidence matrix")
     print(adj_matrix_to_inc_matrix(adj_matrix))
     visualize_graph(adj_matrix)
 
-    visualize_graph(generate_N_P_graph(10, 0.5))
