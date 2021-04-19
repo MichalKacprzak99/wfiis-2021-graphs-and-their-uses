6576a5041b173e66cde99ad25a3964b14fb55a94
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-04-19 16:01:47 +0200

    fix main

--- project1/__main__.py
+++ project1/__main__.py
@@ -3,13 +3,12 @@ from graph_visualization import visualize_graph
 from graph_conversion import *
 
 if __name__ == "__main__":
-    adj_matrix = generate_N_L_graph(1, 0)
+    adj_matrix = generate_N_L_graph(15, 10)
 
     print("Adjacency matrix")
     print(adj_matrix)
     print()
     print("Adjacency list")
-    print(type(adj_matrix_to_list(adj_matrix)))
     print_adj_list(adj_matrix_to_list(adj_matrix))
     print()
     print("Incidence matrix")
