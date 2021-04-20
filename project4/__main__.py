6da45028cfaf561a5ba1150d485c628dad82f7f3
mikeambrozy <mikeyambrozy@gmail.com>
2021-04-21 00:17:24 +0200

    Start working with DFS and kosaraju algorithm

--- project4/__main__.py
+++ project4/__main__.py
@@ -1,5 +1,8 @@
 import digraph_generation
 import graph_conversion as gc
+import kosaraju
 
 adj_matrix = digraph_generation.generate_N_P_digraph(10, 0.1)
-gc.print_adj_list(gc.adj_matrix_to_list(adj_matrix))
+list = gc.adj_matrix_to_list(adj_matrix)
+kosaraju.kosaraju(adj_matrix)
+
--- project4/kosaraju.py
+++ project4/kosaraju.py
@@ -1,8 +1,16 @@
 import numpy as np
 from graph_conversion import adj_matrix_to_list, print_adj_list
+from typing import DefaultDict
 
 
-# def kosaraju(graph: np.ndarray):
+def kosaraju(G: np.ndarray):
+    d = np.full(G.shape[0], -1)
+    f = np.full(G.shape[0], -1)
+    t = 0
+    list = adj_matrix_to_list(G)
+    for row in list:
+        if d[row] == -1:
+            DFS_visit(row, list, d, f, t)
 
 
 # v - wierzchołek;
@@ -12,7 +20,13 @@ from graph_conversion import adj_matrix_to_list, print_adj_list
 # t - czas przetworzenia;
 
 
-def DFS_visit(v: int, G: np.ndarray, d: np.ndarray, f: np.ndarray, t: int):
+def DFS_visit(v: int, G: DefaultDict[int, list], d: np.ndarray, f: np.ndarray, t: int):
     t += 1
     d[v] = t
-
+    for u in G[v]:
+        while True:
+            if d[u] == -1:
+                break
+            DFS_visit(v, G, d, f, t)
+    t = t + 1
+    f[v] = t
