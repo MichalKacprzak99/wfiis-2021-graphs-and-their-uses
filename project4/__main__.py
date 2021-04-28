43e5987c8393d5d4668048c2d6de69b4e37e6a43
mikeambrozy <mikeyambrozy@gmail.com>
2021-04-28 21:47:50 +0200

    Add whole implementation v2

--- project4/__main__.py
+++ project4/__main__.py
@@ -1,8 +1,16 @@
 import digraph_generation
 import graph_conversion as gc
+import numpy as np
 import kosaraju
 
-adj_matrix = digraph_generation.generate_N_P_digraph(10, 0.1)
-list = gc.adj_matrix_to_list(adj_matrix)
+adj_matrix = [[0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 1, 0, 0, 1, 1],
+              [0, 1, 0, 0, 0, 1, 0],
+              [0, 0, 1, 0, 1, 0, 0],
+              [0, 0, 1, 0, 0, 0, 0],
+              [0, 0, 0, 0, 1, 0, 0],
+              [1, 0, 0, 0, 0, 0, 0]]
+adj_list = gc.adj_matrix_to_list(adj_matrix)
+print(adj_list)
+adj_matrix = np.array(adj_matrix)
 kosaraju.kosaraju(adj_matrix)
-
--- project4/kosaraju.py
+++ project4/kosaraju.py
@@ -7,10 +7,21 @@ def kosaraju(G: np.ndarray):
     d = np.full(G.shape[0], -1)
     f = np.full(G.shape[0], -1)
     t = 0
-    list = adj_matrix_to_list(G)
-    for row in list:
-        if d[row] == -1:
-            DFS_visit(row, list, d, f, t)
+    adj_list = adj_matrix_to_list(G)
+    for vertex in adj_list:
+        if d[vertex] == -1:
+            DFS_visit(vertex, adj_list, d, f, t)
+    Gt = np.transpose(G)
+    adj_list = adj_matrix_to_list(Gt)
+    nr = 0
+    comp = np.full(G.shape[0], -1)
+
+    for v in np.sort(f)[::-1]:
+        if comp[v] == -1:
+            nr += 1
+            comp[v] = nr
+            components_r(nr, v, adj_list, comp)
+    print(comp)
 
 
 # v - wierzchołek;
@@ -23,10 +34,19 @@ def kosaraju(G: np.ndarray):
 def DFS_visit(v: int, G: DefaultDict[int, list], d: np.ndarray, f: np.ndarray, t: int):
     t += 1
     d[v] = t
-    for u in G[v]:
-        while True:
-            if d[u] == -1:
+    if v in G:
+        for u in G[v]:
+            if d[u] != -1:
                 break
-            DFS_visit(v, G, d, f, t)
+            DFS_visit(u, G, d, f, t)
     t = t + 1
     f[v] = t
+
+
+def components_r(nr: int, v: int, Gt: DefaultDict[int, list], comp: np.ndarray):
+    if v in Gt:
+        for u in Gt[v]:
+            if comp[u] != -1:
+                break
+            comp[u] = nr
+            components_r(nr, u, Gt, comp)
