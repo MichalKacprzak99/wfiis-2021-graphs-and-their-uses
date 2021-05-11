e86ae723fd541f7c273eedc346e3358a3e1faf8e
mikeambrozy <mikeyambrozy@gmail.com>
2021-05-11 22:47:12 +0200

    Add workin' Kosaraju algorithm

--- project4/__main__.py
+++ project4/__main__.py
@@ -3,13 +3,15 @@ import graph_conversion as gc
 import numpy as np
 import kosaraju
 
-adj_matrix = [[0, 0, 0, 0, 0, 0, 1],
-              [1, 0, 1, 0, 0, 1, 1],
-              [0, 1, 0, 0, 0, 1, 0],
-              [0, 0, 1, 0, 1, 0, 0],
-              [0, 0, 1, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0],
-              [1, 0, 0, 0, 0, 0, 0]]
+adj_matrix = [
+    [0, 1, 1, 0, 1, 0, 0],
+    [1, 0, 1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0]
+]
 adj_list = gc.adj_matrix_to_list(adj_matrix)
 print(adj_list)
 adj_matrix = np.array(adj_matrix)
--- project4/kosaraju.py
+++ project4/kosaraju.py
@@ -1,6 +1,7 @@
 import numpy as np
 from graph_conversion import adj_matrix_to_list, print_adj_list
 from typing import DefaultDict
+import collections
 
 
 def kosaraju(G: np.ndarray):
@@ -10,13 +11,18 @@ def kosaraju(G: np.ndarray):
     adj_list = adj_matrix_to_list(G)
     for vertex in adj_list:
         if d[vertex] == -1:
-            DFS_visit(vertex, adj_list, d, f, t)
+            t = DFS_visit(vertex, adj_list, d, f, t)
     Gt = np.transpose(G)
     adj_list = adj_matrix_to_list(Gt)
     nr = 0
     comp = np.full(G.shape[0], -1)
 
-    for v in np.sort(f)[::-1]:
+    for i in range(G.shape[0]):
+        adj_list[i]
+
+    sorted_by_times_reverse = sorted(adj_list.keys(), key=lambda x: f[x], reverse=True)
+
+    for v in sorted_by_times_reverse:
         if comp[v] == -1:
             nr += 1
             comp[v] = nr
@@ -31,22 +37,21 @@ def kosaraju(G: np.ndarray):
 # t - czas przetworzenia;
 
 
-def DFS_visit(v: int, G: DefaultDict[int, list], d: np.ndarray, f: np.ndarray, t: int):
+def DFS_visit(v: int, G: DefaultDict[int, list], d: np.ndarray, f: np.ndarray, t: int) -> int:
     t += 1
     d[v] = t
     if v in G:
         for u in G[v]:
-            if d[u] != -1:
-                break
-            DFS_visit(u, G, d, f, t)
+            if d[u] == -1:
+                t = DFS_visit(u, G, d, f, t)
     t = t + 1
     f[v] = t
+    return t
 
 
 def components_r(nr: int, v: int, Gt: DefaultDict[int, list], comp: np.ndarray):
     if v in Gt:
         for u in Gt[v]:
-            if comp[u] != -1:
-                break
-            comp[u] = nr
-            components_r(nr, u, Gt, comp)
+            if comp[u] == -1:
+                comp[u] = nr
+                components_r(nr, u, Gt, comp)
