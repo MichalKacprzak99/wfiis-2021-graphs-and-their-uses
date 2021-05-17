1a055a28dc007848e90913811e7ed557bd98e019
mikolajczykb <uncharted.mikolajczyk4@gmail.com>
2021-05-17 15:01:28 +0200

    finish up project2

--- grafy/test/project2/test_euler.py
+++ grafy/test/project2/test_euler.py
@@ -7,12 +7,12 @@ class CheckIfEulerTest(unittest.TestCase):
 
     def test_negative_amount_of_vertices_get_euler_graph(self):
         amount_of_vertices = -3
-        err_message = "N has to be greater than 0"
+        err_message = "N has to be greater than 2"
         self.assertRaisesRegex(ValueError, err_message, get_euler_graph, amount_of_vertices)
 
     def test_no_vertices_get_euler_graph(self):
         amount_of_vertices = 0
-        err_message = "N has to be greater than 0"
+        err_message = "N has to be greater than 2"
         self.assertRaisesRegex(ValueError, err_message, get_euler_graph, amount_of_vertices)
 
     def test_positive_amount_of_vertices_get_euler_graph(self):
