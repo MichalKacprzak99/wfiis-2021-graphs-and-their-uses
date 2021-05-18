26293c547af1b103f93c6d3bba5095bc3bb87f9a
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-05-18 20:19:54 +0200

    add visualization to example usage

--- grafy/project3/__main__.py
+++ grafy/project3/__main__.py
@@ -18,12 +18,10 @@ def print_array(arr):
     for a in arr:
         for elem in a:
             print(f"{elem:<2.0f}".rjust(3), end="")
-            # print("{:.0f}".format(elem).rjust(3), end="")
         print(end="\n")
 
 
 if __name__ == "__main__":
-
     random_connected_graph = generate_connected_graph(12)
     print("Adjacency matrix\n")
     print_array(random_connected_graph)
@@ -40,4 +38,4 @@ if __name__ == "__main__":
     print(f"Mnimax graph centers: {np.array2string(graph_minimax_center, precision=2, separator=', ')[1:-1]}\n")
     print("Minimal spanning tree")
     kruskal_mst(random_connected_graph)
-    # visualize_graph(random_connected_graph, weighted_graph=True)
+    visualize_graph(random_connected_graph, weighted_graph=True)
