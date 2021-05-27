7c4451b50ba15adb6faf060f012b0c162851af70
mikolajczykb <uncharted.mikolajczyk4@gmail.com>
2021-05-27 03:47:36 +0200

    upload project 4's johnson's algorithm

--- grafy/project4/__main__.py
+++ grafy/project4/__main__.py
@@ -1,13 +1,40 @@
+import sys
 from numpy import all
 
-import bellman_ford
-import kosaraju
-from grafy.project3 import connected_graph_generation
+sys.path.append('../..')
 
-adj_matrix = connected_graph_generation.generate_connected_graph(7)
+from grafy.project4 import kosaraju
+from grafy.project4 import bellman_ford
+from grafy.project4 import digraph_generation
+from grafy.project4 import johnson
+from grafy.project1 import graph_visualization
+
+adj_matrix = digraph_generation.generate_N_P_digraph(7, 0.5)
+print(adj_matrix)
 graph_connected_components = kosaraju.kosaraju(adj_matrix)
-print(graph_connected_components)
-if all(graph_connected_components == 1):
-    print("Graph has one connected component!")
 
-print(bellman_ford.bellman_ford(adj_matrix, 0))
+weighted_adj_matrix = bellman_ford.matrix_assign_weights(adj_matrix)
+cycle_info = bellman_ford.bellman_ford(adj_matrix, weighted_adj_matrix, 0)
+counter = 1
+
+while all(graph_connected_components == 1) != 1 or cycle_info[0] is not True:
+    adj_matrix = digraph_generation.generate_N_P_digraph(7, 0.5)
+    graph_connected_components = kosaraju.kosaraju(adj_matrix)
+
+    weighted_adj_matrix = bellman_ford.matrix_assign_weights(adj_matrix)
+    cycle_info = bellman_ford.bellman_ford(adj_matrix, weighted_adj_matrix, 0)
+    counter = counter + 1
+
+print(" ")
+print(graph_connected_components)
+print("Graph has one connected component")
+print("...after", counter, "tries.")
+
+print(" ")
+print(cycle_info)
+
+D = johnson.johnson(adj_matrix, weighted_adj_matrix)
+for row in D:
+    print(row)
+graph_visualization.visualize_graph(weighted_adj_matrix, True, True)
+
--- grafy/project4/bellman_ford.py
+++ grafy/project4/bellman_ford.py
@@ -1,20 +1,48 @@
 import numpy as np
 from grafy.project3.dijkstra_algorithm import init, relax
 from grafy.project1 import graph_conversion
+from typing import Tuple, Union, List, Dict
+import copy
+import random
 
 
-def bellman_ford(graph_matrix: np.ndarray, start_vertex: int) -> bool:
-    vertices_number, _ = graph_matrix.shape
-    d_s, p_s = init(vertices_number, start_vertex)
-    adj_list = graph_conversion.adj_matrix_to_list(graph_matrix)
+def matrix_assign_weights(adj_matrix: np.ndarray) -> np.ndarray:
+    adj_matrix_weighted = copy.deepcopy(adj_matrix)
+    for i in range(len(adj_matrix)):
+        for j in range(len(adj_matrix[0])):
+            if adj_matrix[i][j] == 1:
+                adj_matrix_weighted[i][j] = random.randint(-5, 11)
 
-    for i in range(1, vertices_number - 1):
+    return adj_matrix_weighted
+
+
+def wmatrix_to_wlist(adj_matrix_weighted: np.ndarray) -> List[List[Union[int, Dict[str, float]]]]:
+    weighted_list = []
+    for i in range(len(adj_matrix_weighted)):
+        for j in range(len(adj_matrix_weighted[0])):
+            if adj_matrix_weighted[i][j] != 0:
+                weighted_list.append([i+1, j+1, {'weight': adj_matrix_weighted[i][j]}])
+    return weighted_list
+
+# G - graph matrix, w - weighted matrix, s - start vertex
+
+
+def bellman_ford(G: np.ndarray, w: np.ndarray, s: int) -> Tuple[bool, np.ndarray, np.ndarray]:
+
+    if G.shape != w.shape:
+        raise ValueError("The graph matrix and weighted matrix are of different shapes")
+
+    vertices_number, _ = G.shape
+    d_s, p_s = init(vertices_number, s)
+    adj_list = graph_conversion.adj_matrix_to_list(G)
+
+    for i in range(vertices_number - 1):
         for u in range(vertices_number):
             for v in adj_list[u]:
-                relax(u, v, graph_matrix[u][v], d_s, p_s)
+                relax(u, v, w[u][v], d_s, p_s)
 
     for u in range(vertices_number):
         for v in adj_list[u]:
-            if d_s[v] > d_s[u] + graph_matrix[u][v]:
-                return False
-    return True
+            if d_s[v] > d_s[u] + w[u][v]:
+                return False, d_s, p_s
+    return True, d_s, p_s
