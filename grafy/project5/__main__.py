eeba81ec184e9c6e2249506bf6fcf627cc643723
Kacper Łuka <65722563+Kannute@users.noreply.github.com>
2021-05-30 12:52:32 +0200

    Update __main__.py

--- grafy/project5/__main__.py
+++ grafy/project5/__main__.py
@@ -1,6 +1,10 @@
 from grafy.project5.flow_network import generate_flow_network
+from grafy.project5.ford_fulkerson import ford_fulkerson
 from grafy.project1.graph_visualization import visualize_graph
+
 if __name__ == '__main__':
     flow_network = generate_flow_network(2)
     print(flow_network)
     visualize_graph(flow_network, True, True)
+    # max_flow_network = ford_fulkerson(flow_network, 0, 5)
+    # visualize_graph(max_flow_network, T
