fd65c3e5710318457bf5c9b1c6ac6284d32da7b2
Kacper Łuka <65722563+Kannute@users.noreply.github.com>
2021-05-30 12:52:55 +0200

    Update __main__.py

--- grafy/project5/__main__.py
+++ grafy/project5/__main__.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     print(flow_network)
     visualize_graph(flow_network, True, True)
     # max_flow_network = ford_fulkerson(flow_network, 0, 5)
-    # visualize_graph(max_flow_network, T
+    # visualize_graph(max_flow_network, True, True)
