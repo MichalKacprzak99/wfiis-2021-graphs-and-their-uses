b210a2901f60506300d6c59e925a5111f7864385
mikolajczykb <uncharted.mikolajczyk4@gmail.com>
2021-06-05 01:10:27 +0200

    fix project 4

--- grafy/project4/__main__.py
+++ grafy/project4/__main__.py
@@ -3,31 +3,18 @@ import sys
 sys.path.append('../..')
 
 from grafy.project4.kosaraju import kosaraju
-from grafy.project4.bellman_ford import bellman_ford, assign_weights_to_adj_matrix
+from grafy.project4.bellman_ford import bellman_ford, assign_weights_to_adj_matrix, generate_connected_digraph
 from grafy.project4.digraph_generation import generate_N_P_digraph
 from grafy.project4.johnson import johnson
 from grafy.project1.graph_visualization import visualize_graph
+from grafy.project5.__main__ import print_array
 
-vertice_number = 7
+
+vertice_amount = 7
 probability = 0.5
 
-adj_matrix = generate_N_P_digraph(vertice_number, probability)
-graph_connected_components = kosaraju(adj_matrix)
-
-weighted_adj_matrix = assign_weights_to_adj_matrix(adj_matrix)
-cycle_info = bellman_ford(adj_matrix, weighted_adj_matrix, 0)
-counter = 1
-
-while len(graph_connected_components) != 1 or cycle_info[0] is not True:
-    adj_matrix = generate_N_P_digraph(vertice_number, probability)
-    graph_connected_components = kosaraju(adj_matrix)
-
-    weighted_adj_matrix = assign_weights_to_adj_matrix(adj_matrix)
-    cycle_info = bellman_ford(adj_matrix, weighted_adj_matrix, 0)
-    counter = counter + 1
-
-print("Generated digraph:")
-print(adj_matrix)
+adj_matrix, weighted_adj_matrix, graph_connected_components, cycle_info = generate_connected_digraph(vertice_amount,
+                                                                                                     probability)
 
 print("Digraph with weights:")
 print(weighted_adj_matrix)
@@ -35,11 +22,17 @@ print(weighted_adj_matrix)
 print(" ")
 print(graph_connected_components)
 print("Graph has one connected component")
-print("...after", counter, "tries.")
+
 
 print(" ")
 print("Bellman Ford algorithm:")
-print(cycle_info)
+print("No negative cycles found: ", cycle_info[0])
+print("Array of shortest paths:")
+print(cycle_info[1])
+print()
+print("Array of previous nodes in shortest paths:")
+print(cycle_info[2])
+
 
 print(" ")
 print("Johnson algorithm:")
